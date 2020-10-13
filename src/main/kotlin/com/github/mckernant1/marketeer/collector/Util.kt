da40b7a1f714fcb2e6d4d76b8a2ff57c3f3eb234
Tom McKernan <tmeaglei@gmail.com>
2020-10-13 10:58:53 -0700

    to connection string

--- src/main/kotlin/com/github/mckernant1/marketeer/collector/Util.kt
+++ src/main/kotlin/com/github/mckernant1/marketeer/collector/Util.kt
@@ -8,16 +8,12 @@ fun getStringFromFile(filename: String): String =
 
 
 
-private val dbUser: String = System.getenv("MONGO_USER")
+private val connectionString: String = System.getenv("MONGO_CONNECTION_STRING")
         ?: throw Exception("Environment variable MONGO_USER has not been specified")
-private val dbPassword: String = System.getenv("MONGO_PASSWORD")
-        ?: throw Exception("Environment variable MONGO_PASSWORD has not been specified")
 
-private val wordDatabase = "words"
-
-private val client =
-    KMongo.createClient("mongodb+srv://$dbUser:$dbPassword@marketeer.h1pdv.mongodb.net/$wordDatabase?retryWrites=true&w=majority")
-val collection = client.getDatabase("words").getCollection<WordDay>("words")
+private val client = KMongo.createClient(connectionString)
+val collection =
+    client.getDatabase("words").getCollection<WordDay>("words")
 
 
 data class WordDay(
